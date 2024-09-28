42481c1b722b24f31ef579c586a19a32c3603762
Mohaiminul375 <mohaiminul375@gmail.com>
2024-09-28 15:53:57 +0600

    show data in UI

--- src/app/posts/page.jsx
+++ src/app/posts/page.jsx
@@ -1,20 +1,22 @@
-import React from 'react';
-// fetch data
-const getPosts=async()=>{
-    const res=await fetch('https://jsonplaceholder.typicode.com/posts')
-    const data=await res.json();
-    return data;
-}
+import React from "react";
+import { getPosts } from "../services/postApi";
 
+const page = async () => {
+  const postsData = await getPosts();
 
-const page = async() => {
-    const postsData=await getPosts();
-    console.log(postsData);
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <div className="">
+      <h5 className="my-10 text-center text-3xl">All Posts</h5>
+      <div className="grid grid-cols-4 gap-6">
+        {postsData?.slice(0, 10)?.map(({ title, userId, body }) => (
+          <div key={userId} className="border-2 p-6">
+            <h6>{title}</h6>
+            <h6>{body}</h6>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default page;
---
+++ src/app/services/postApi.js
@@ -0,0 +1,6 @@
+// fetch data
+export const getPosts = async () => {
+    const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+    const data = await res.json();
+    return data;
+};
